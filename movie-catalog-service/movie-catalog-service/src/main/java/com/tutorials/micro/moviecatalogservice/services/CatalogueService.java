aba21940bd7bfa0cd8b6fa4509274d2cb7c8cb86
Tapiwa Pitso <tapiwapitso@gmail.com>
2020-08-11 11:49:25 +0200

    Added Hystrix properties

--- movie-catalog-service/movie-catalog-service/src/main/java/com/tutorials/micro/moviecatalogservice/services/CatalogueService.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/tutorials/micro/moviecatalogservice/services/CatalogueService.java
@@ -1,6 +1,7 @@
 package com.tutorials.micro.moviecatalogservice.services;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.tutorials.micro.moviecatalogservice.model.CatalogueItem;
 import com.tutorials.micro.moviecatalogservice.model.Movie;
 import com.tutorials.micro.moviecatalogservice.model.Rating;
@@ -14,14 +15,20 @@ public class CatalogueService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogueItem")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogueItem",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            }
+    )
     public CatalogueItem getCatalogueItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogueItem(movie.getName(), "Description", rating.getRating());
     }
 
     private CatalogueItem getFallbackCatalogueItem(Rating rating) {
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogueItem("No Movie", "No Description", rating.getRating());
     }
 }
--- movie-catalog-service/movie-catalog-service/src/main/java/com/tutorials/micro/moviecatalogservice/services/RatingService.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/tutorials/micro/moviecatalogservice/services/RatingService.java
@@ -1,6 +1,7 @@
 package com.tutorials.micro.moviecatalogservice.services;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.tutorials.micro.moviecatalogservice.model.Rating;
 import com.tutorials.micro.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,14 @@ public class RatingService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            }
+    )
     public UserRating getUserRating(String userId) {
         UserRating userRating = restTemplate.getForObject("http://movie-ratings-service/ratingsdata/user/" + userId, UserRating.class);
         return userRating;
